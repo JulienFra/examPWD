9f0dd5759edaa8d2234a52b8c56824c249abb497
Tr4ckTV <theo.a.gregoire@gmail.com>
2024-02-25 15:15:28 +0100

    Etudiant

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,26 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great! uwu
+|
+*/
 use App\Http\Controllers\AdminActionsController;
 use App\Http\Controllers\StudentController;
 
 Route::get('/', function () {
     return redirect()->route('admin.actions');
-})->name('home');
+})->middleware(['auth'])->name('home');
 
 Route::get('/admin-actions', [AdminActionsController::class, 'index'])
+    ->middleware(['auth'])
     ->name('admin.actions');
 
 Route::get('/students', [StudentController::class, 'index'])->name('students.index');
